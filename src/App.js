04b844cc8bd4207f9778953333e283b394df419e
Amir Lebba <amirlebba}gmail.com>
2025-02-20 16:00:46 +0100

    Deploying

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Upload from "./upload/Upload";
 import VideoBrowser from "./Browse/VideoBrowser";
 import VPlayer from "./Player/VPlayer";
